dd7671a001153fb4af04484fdc7f61400efba7d1
theangelsway <94010719+theangelsway@users.noreply.github.com>
2021-11-09 13:34:37 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 beg= True
 pink= False
-buyj= True
+buyj= False
 hunt= True
 fish= True
 pm= True
-bet= True
-slots= True
-hl= True
+bet= False
+slots= False
+hl= False
 dep= True
 #id of the user reciving the pink
 send_id =270904126974590976  #dank memer's id
